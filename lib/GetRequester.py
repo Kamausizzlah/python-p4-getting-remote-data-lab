b7d487001965e7858ff02a2b82aefb5c16bc1551
Ian Kamau <iankamau112@gmail.com>
2024-07-18 14:01:51 +0300

    commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,13 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
         pass
+        response_body = self.get_response_body()
+        data = json.loads(response_body)
+        return data
